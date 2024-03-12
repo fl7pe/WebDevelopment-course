59e0b5dd13408f5431e335f17c5a09c67381bc46
fl7pe <115465197+fl7pe@users.noreply.github.com>
2024-03-12 00:04:19 -0300

    Challenge completed

--- 20.0 Simon Game Challenge/index.js
+++ 20.0 Simon Game Challenge/index.js
@@ -1,28 +1,105 @@
-var gameStatus = "waiting";
-var Level = 1;
+var gameStatus = "waitingToStart";
+var randomChosenColour;
 var newNumber;
-var array = [];
+var level = 0;
+var i = 0;
+var userClicks = 0;
+var buttonColours = ["green", "red", "yellow", "blue"];
+var pattern = [];
+var userClickedPattern = [];
+var isCorrect;
+
+
+function playSound(name) {
+    var audio = new Audio('./sounds/'+name+'.mp3');
+    audio.play();
+}
+
+function animatePress(currentColour) {
+    $("#" + currentColour).fadeToggle(100).fadeToggle(100);
+    $("#" + currentColour).addClass("pressed");
+setTimeout(function() {
+    $("#" + currentColour).removeClass("pressed");
+}, 100);
+
+}
 
 function startNewGame() {
-    $("h1").text("New Game");
-    gameStatus = "crying";
+    pattern = [];
+    i=0;
+    level = 0;
+    userClickedPattern = [];
+    newLevel();
+    gameStatus = "sweating";
 }
 
 function gameOver() {
-    $("h1").text("Game Over");
-    gameStatus = "waiting";
-}
-
-function newLevel() {
-    newNumber = Math.floor(Math.random() * 4) + 1;
-    array.push(newNumber);
+    $("h1").text("Game Over, Press Any Key to Restart");
+    $("body").addClass("game-over");
+setTimeout(function() {
+    $("body").removeClass("game-over");
+}, 100);
     
 }
 
+function newLevel() {
+    level++;
+    $("h1").text("Level " + level);
+    newNumber = Math.floor(Math.random() * 4);
+    randomChosenColour = buttonColours[newNumber];
+    pattern.push(randomChosenColour);
+    animatePress(randomChosenColour);
+    playSound(randomChosenColour);
+    
+    
+}
+
+function checkAnswer(currentLevel) {
+
+    if (i < currentLevel) {
+        if (pattern[i] === userClickedPattern[i]) {
+            isCorrect = true; 
+            i++;
+        } else {
+            isCorrect = false;
+            return isCorrect
+    }
+
+    } else {
+        i = 0;
+    }
+return isCorrect
+}
+
 $(document).keypress(function(event) {
-   if (gameStatus === "waiting") {
+    console.log(gameStatus);
+   if (gameStatus === "waitingToStart") {
         startNewGame();
-   } else {
-        gameOver();
-   } 
+   }
+});
+
+$(".btn").click(function() {
+    var userChosenColour = this.id;
+    userClickedPattern.push(userChosenColour);
+    checkAnswer(level);
+    if (isCorrect===true) {
+        playSound(userChosenColour);
+        animatePress(userChosenColour);
+        if (pattern.length === userClickedPattern.length) {
+        setTimeout(function() {
+            newLevel();
+            i=0;
+        }, 1000);
+        userClickedPattern = [];
+        }
+    } else if ( $("h1").html() === "Press A Key to Start") {
+
+} else {
+    gameStatus = "waitingToStart";
+    playSound("wrong");
+    animatePress(userChosenColour);
+    gameOver();
+    
+}
+
 });
